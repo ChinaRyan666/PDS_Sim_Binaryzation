a07cf3df9c7f737df662584d6ba265334bcbfa09
Ryan Cai <caijietao888@163.com>
2023-10-25 12:58:25 +0800

    v1

--- Sim_Binaryzation/prj/sim/behav/run_behav.bat
+++ Sim_Binaryzation/prj/sim/behav/run_behav.bat
@@ -1,6 +1,6 @@
 @echo off
 set bin_path=C:/modelsim_dlx64_10.6c/win64pe
-cd D:/Github/2023FPGARace-0285/Reference Design/wolf code/2_txt_sim/2_txt_sim/prj/sim/behav
+cd D:/Github/PDS_Sim_Binaryzation/Sim_Binaryzation/prj/sim/behav
 call "%bin_path%/modelsim"   -do "do {run_behav_compile.tcl};do {run_behav_simulate.tcl}" -l run_behav_simulate.log
 if "%errorlevel%"=="1" goto END
 if "%errorlevel%"=="0" goto SUCCESS
